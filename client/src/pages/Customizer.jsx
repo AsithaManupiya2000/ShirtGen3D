a95660623b6c4cd65db3dd96125fdae0e2052527
AsithaManupiya2000 <asithamanupiya@gmail.com>
2023-07-11 14:43:12 +0530

    customizer tab added

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import { AnimatePresence,motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import {download} from '../assets';
+import{downloadCanvasToImage,reader} from '../config/helpers';
+import {EditorTabs,FilterTabs,DecalTypes} from '../config/constants';
+import { fadeAnimation,slideAnimation } from '../config/motion';
 
 const Customizer = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <> Customizer
+        </>
+      )}
+    </AnimatePresence>
   );
 }
 
