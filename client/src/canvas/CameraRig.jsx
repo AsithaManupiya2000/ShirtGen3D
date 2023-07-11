394868571aae3fd6c45b0de1d3a686584c292f89
AsithaManupiya2000 <asithamanupiya@gmail.com>
2023-07-12 03:02:02 +0530

    full texture and logo texture added

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,18 +1,42 @@
-import React,{useRef} from 'react';
+import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
-import {easing} from 'maath';
+import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
 
-const CameraRig = ({childern}) => {
-    const group = useRef();
-    const snap = useSnapshot(state);
-  return (
-    <group ref={group}>
-        {childern}
-    </group>
-  );
-};
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // model initial position setting
+    let targetPosition = [-0.4, 0, 2];
+    if(snap.intro) {
+      if(isBreakpoint) targetPosition = [0, 0, 2];
+      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if(isMobile) targetPosition = [0, 0, 2.5]
+      else targetPosition = [0, 0, 2];
+    }
+
+    //model camera position setting
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+
+    // model rotation setting
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
+
+
+  return <group ref={group}>{children}</group>
+}
 
 export default CameraRig;
--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -21,7 +21,24 @@ const Shirt = () => {
             material-roughness={1}
             dispose={null}
         >
+            {snap.isFullTexture &&(
+                <Decal
+                    position={[0,0.04,0.15]}
+                    rotation={[0,0,0]}
+                    scale={0.15}
+                    map={logoTexture}
+                />
 
+            )}
+
+            {snap.isLogoTexture &&(
+                <Decal
+                    position={[0,0.04,0.15]}
+                    rotation={[0,0,0]}
+                    scale={0.15}
+                    map={logoTexture}
+                />    
+            )}
         </mesh>
     </group>
   )
